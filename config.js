73f24e00f7eefda20d09ae924d8c137b9df6e62f
Afridi2001 <151954469+Afridi2001@users.noreply.github.com>
2024-01-10 21:06:11 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+export const PORT = 5000;
+
+export const mongoDBURL =  'mongodb+srv://asimafridi:asim6565@cluster1.qzd3d0w.mongodb.net/';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
